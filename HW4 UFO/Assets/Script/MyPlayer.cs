b93a05c9e33526df7088f9e7593c8187784fe1fb
Sadi <3108987378@qq.com>
2018-05-12 01:34:12 +0800

    added hw6

---
+++ HW4 UFO/Assets/Script/MyPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MyPlayer : MonoBehaviour
+{
+    public int armor = 50;
+    public int damage = 50;
+    public GameObject gun;
+
+    void Update()
+    {
+    
+    }
+}
