0697bca02199c4a66ef00f8d6ea3d52a6bf15122
Sadi <3108987378@qq.com>
2018-05-29 22:59:21 +0800

    add hw7

---
+++ HW7/ParticleSystem_CarExhaust/Assets/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+        public GameObject Car;
+        private Vector3 offset;
+        
+	// Use this for initialization
+	void Start () {
+	offset = transform.position - Car.transform.position;	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+	transform.position = Car.transform.position + offset;	
+	}
+}
