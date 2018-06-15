9f2e30fb5efdb04a24b68cafcf4656cf891fa41e
Sadi <3108987378@qq.com>
2018-06-15 21:21:27 +0800

    hw9

---
+++ HW9/AI Tank/Assets/Scripts/MyScripts/Chase.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chase : MonoBehaviour
+{
+    public Transform player;
+    static Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Vector3.Distance(player.position, this.transform.position) < 15)
+        {
+            Vector3 direction = player.position - this.transform.position;
+            direction.y = 0;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 50f);
+
+            anim.SetBool("isIdle", false);
+            if (direction.magnitude > 5)
+            {
+                this.transform.Translate(0, 0, 2f);
+                anim.SetBool("isRunning", true);
+                
+            }
+           
+        }
+    }
+}
---
+++ HW9/README.md
@@ -0,0 +1,2 @@
+[AI-Tank-Zombie demo-attack](http://www.iqiyi.com/w_19rz1vwexh.html)  
+[MR-VirtualButton-Image_Recognition-demo](http://www.iqiyi.com/w_19ryx5d5dx.html)
