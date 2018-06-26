9effd727c9cfc2e3dd87f44c4c0ee970d6ad09fc
Sadi <3108987378@qq.com>
2018-06-26 17:15:31 +0800

    add HW10

---
+++ HW10/Assets/Scripts/PlayerObject.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerObject : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if(isLocalPlayer == false)
+        {
+            return;
+        }
+
+        CmdSpawnMyUnit();
+       // Instantiate(PlayerUnitPrefab);
+
+
+    }
+    public GameObject PlayerUnitPrefab;
+	// Update is called once per frame
+	void Update () {
+        if (isLocalPlayer == false)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            CmdSpawnMyUnit();
+        } 
+    }
+    GameObject myPlayerUnit;
+
+    [Command]
+    void CmdSpawnMyUnit()
+    {
+        GameObject go = Instantiate(PlayerUnitPrefab);
+        myPlayerUnit = go;
+        //go.GetComponent<NetworkIdentity>().AssignClientAuthority(connectionToClient);
+        NetworkServer.SpawnWithClientAuthority(go, connectionToClient);
+    }
+
+    [Command]
+    void CmdMoveUnitUp()
+    {
+        if(myPlayerUnit == null)
+        {
+            return;
+        }
+        myPlayerUnit.transform.Translate(0, 1, 0);
+    }
+}
---
+++ HW10/README.md
@@ -0,0 +1,2 @@
+##Multiplayer game(UNet)  
+###[Watch demo](http://www.iqiyi.com/w_19rymoyxi9.html)
